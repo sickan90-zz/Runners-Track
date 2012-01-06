1834b8dc5c7a59290e4e8009472ac2830dafc383
Erik Sikander <erik.sikander@gmail.com>
2012-01-06 15:12:47 +0100

    Added the project folder. Also removed the folder 'bin' and 'gen' by adding it to the .gitignore file

---
+++ Runners Track/src/se/sickan90/runnerstrack/RunnersTrackActivity.java
@@ -0,0 +1,13 @@
+package se.sickan90.runnerstrack;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RunnersTrackActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
